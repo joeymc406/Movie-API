d8e55cab1d527d4a7dc7ac67371954e4cbc39bcf
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-11-30 16:07:10 -0700

    removed cors and allowed origins added simple cors

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true
 }));
 
-let allowedOrigins = ['http://localhost:8080', 'https://joeymc406movie-api.herokuapp.com/', 'http://localhost:1234'];
+//let allowedOrigins = ['http://localhost:8080', 'https://joeymc406movie-api.herokuapp.com/', 'http://localhost:1234'];
 //possible issue regarding origins!!! added heroku url
 
 const cors = require('cors');
-app.use (cors({
-      origin: (origin, callback) => {
-            if(!origin) return callback(null, true);
-            if(allowedOrigins.indexOf(origin) === -1){
-                  //if a specific origin isn't found on the list of allowed origins
-                  let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-                  return callback(new Error(message ), false);
-            }
-            return callback(null, true);
-      }
-}));
-
+// app.use (cors({
+//       origin: (origin, callback) => {
+//             if(!origin) return callback(null, true);
+//             if(allowedOrigins.indexOf(origin) === -1){
+//                   //if a specific origin isn't found on the list of allowed origins
+//                   let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//                   return callback(new Error(message ), false);
+//             }
+//             return callback(null, true);
+//       }
+// }));
+app.use(cors())
 let auth = require('./auth')(app);
 
 const passport = require('passport');
